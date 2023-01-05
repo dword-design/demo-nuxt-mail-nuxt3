2b61c3bc1897ff5da6e324e7dd8e3295135e9599
Sebastian Landwehr <sebastian.landwehr@honestly.com>
2023-01-05 16:22:29 +0100

    feat: init

---
+++ nuxt.config.js
@@ -0,0 +1,11 @@
+import dotenv from '@dword-design/dotenv-json-extended'
+
+dotenv.config()
+
+export default defineNuxtConfig({
+  modules: [
+    ['nuxt-mail', {
+      smtp: JSON.parse(process.env.MAIL_CONFIG),
+    }],
+  ],
+})
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <button @click="send">Send email</button>
+</template>
+
+<script setup>
+const { $mail } = useNuxtApp()
+const send = () => $mail.send({
+  from: 'John Doe',
+  to: 'foo@bar.de',
+  subject: 'Incredible',
+  text: 'This is an incredible test message',
+})
+</script>
